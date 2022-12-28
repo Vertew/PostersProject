b9515e83a66d16e742d1337976730a1cf5bf770e
Sam Tudberry <sam@tudberry.net>
2022-12-28 19:56:07 +0000

    Improved visuals of livewire forms

--- resources/views/livewire/like-form.blade.php
+++ resources/views/livewire/like-form.blade.php
@@ -1,8 +1,4 @@
-@push('styles')
-    <link rel="stylesheet" href="{{asset('/css/app.css')}}">
-@endpush
-
-<p style='margin: 0px' >
+<p style='margin: 0px'>
 
 {{ $likes }} Likes <button wire:click="increment">Like</button>
 
--- resources/views/livewire/notification-form.blade.php
+++ resources/views/livewire/notification-form.blade.php
@@ -1,28 +1,27 @@
-@push('styles')
-    <link rel="stylesheet" href="{{asset('/css/app.css')}}">
-@endpush
-
 <div>
     <button wire:click="mark_all_read">Mark all as read</button>
         <ul>
             @forelse ($user->unreadNotifications as $notification)
+                <li style = 'text-align: center'><button  wire:click="mark_read('{{$notification->id}}')">Mark as read</button></li>
                 @if ($notification->type == "App\Notifications\PostLiked")
-                    <li>[{{ $notification->created_at }}] 
-                        <a href="{{route('users.show', ['id'=> $notification->data['user_id']])}}">{{$notification->data['username']}}</a> liked your post: 
+                    <li style = 'text-align: center'>
+                        <a href="{{route('users.show', ['id'=> $notification->data['user_id']])}}">{{$notification->data['username']}} liked your post: </a>
                         <a href = "{{route('posts.show', ['id'=>$notification->data['post_id']])}}">{{$notification->data['title']}}</a>
+                        {{ $notification->created_at }}
                     </li>
                 @elseif ($notification->type == "App\Notifications\CommentReceived")
-                    <li>[{{ $notification->created_at }}] 
-                        <a href="{{route('users.show', ['id'=> $notification->data['user_id']])}}">{{$notification->data['username']}}</a> commented on your post: 
+                    <li style = 'text-align: center'>
+                        <a href="{{route('users.show', ['id'=> $notification->data['user_id']])}}">{{$notification->data['username']}} commented on your post:</a>
                         <a href = "{{route('posts.show', ['id'=>$notification->data['post_id']])}}">{{$notification->data['title']}}</a>
+                        {{ $notification->created_at }}
                     </li>
                 @elseif ($notification->type == "App\Notifications\CommentViewed")
-                    <li>[{{ $notification->created_at }}] 
-                        <a href="{{route('users.show', ['id'=> $notification->data['user_id']])}}">{{$notification->data['username']}}</a> saw your comment!: 
+                    <li style = 'text-align: center'>
+                        <a href="{{route('users.show', ['id'=> $notification->data['user_id']])}}">{{$notification->data['username']}} saw your comment! </a> 
                         <a href = "{{route('comments.show', ['id'=>$notification->data['comment_id']])}}">{{$notification->data['comment_text']}}</a>
+                        {{ $notification->created_at }}
                     </li>
                 @endif
-                <button wire:click="mark_read('{{$notification->id}}')">Mark as read</button>
             @empty
                 No new notifications at the moment.
             @endforelse
